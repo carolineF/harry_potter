07404b5d8c0c549a3b1c98d5805ad4f324b74f86
carolineF <1138483832@qq.com>
2015-09-09 21:37:15 +0800

    add method addBasketItem and findBasketItem in basket model

--- src/model/basket.js
+++ src/model/basket.js
@@ -4,4 +4,24 @@ function Basket(basketItems) {
   this.basketItems = basketItems || [];
 }
 
+Basket.prototype.addBasketItem = function(item) {
+
+  var basketItem = this.findBasketItem(item);
+  if(basketItem) {
+    basketItem.count += item.count;
+  }else{
+    this.basketItems.push(item);
+  }
+};
+
+Basket.prototype.findBasketItem = function(item) {
+  this.basketItems.forEach(function(basketItem) {
+
+    var isExist = basketItem.book.title === item.book.title;
+    if(isExist) {
+      return basketItem;
+    }
+  });
+};
+
 module.exports = Basket;
