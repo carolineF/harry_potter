9567c3d22c371a1c326db6cab8d3c8d991ad1733
carolineF <1138483832@qq.com>
2015-09-12 16:57:54 +0800

    add seed basket-items.js

---
+++ seed/basket-items.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = [
+  {book: {title: 'Harry Potter I', price: 8}, count: 2},
+  {book: {title: 'Harry Potter II', price: 8}, count: 2},
+  {book: {title: 'Harry Potter III', price: 8}, count: 2},
+  {book: {title: 'Harry Potter VI', price: 8}, count: 1},
+  {book: {title: 'Harry Potter V', price: 8}, count: 1}
+];
